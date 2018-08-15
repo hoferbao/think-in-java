b741b461ae84ec5ee3f4e0875a38d5995ff63ec5
hoferbao <hoferbao@gmail.com>
2018-08-15 20:25:15 +0800

    helloworld

--- src/main/java/com/hofer/tij/common/HelloWorld.java
+++ src/main/java/com/hofer/tij/common/HelloWorld.java
@@ -6,6 +6,20 @@ package com.hofer.tij.common;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        /**
+         * http://baike.corp.taobao.com/index.php/MemberPlatform/userTagWiki#promotedType
+         * userTag3 2的49次方 标记此用户是否为分销平台的供应商
+         */
+        final long SUPPLIER = 1L << 49;
+
+        /**
+         * userTag4 2的44次方 标记此用户是分销平台的品牌商
+         */
+        final long BRAND_SELLER = 1L << 44;
+        long userTag3 = 72691599965995136L;
+        long userTag4 = 1166511468326421504L;
+        System.out.println((userTag3 & SUPPLIER) == SUPPLIER);
+        System.out.println((userTag4 & BRAND_SELLER) == BRAND_SELLER);
+        boolean isSupplier = ((userTag3 & SUPPLIER) == SUPPLIER) || ((userTag4 & BRAND_SELLER) == BRAND_SELLER);
     }
 }
