f1835b490d3810a4c81dc77254f2e0657dbfa61f
hoferbao <hoferbao@gmail.com>
2018-11-28 20:33:26 +0800

    two sum

--- src/main/java/leetcode/p001/TwoSum.java
+++ src/main/java/leetcode/p001/TwoSum.java
@@ -2,6 +2,7 @@ package leetcode.p001;
 
 /**
  * https://leetcode-cn.com/problems/two-sum/
+ *
  * @author hofer.bhf
  * created on 2018/11/28 8:13 PM
  */
@@ -16,10 +17,11 @@ public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         int i = 0;
         int j = 1;
+        OK:
         for (; i < nums.length - 1; i++) {
             for (j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    break;
+                    break OK;
                 }
             }
 
