fd7020cd05de85a1c26f4ddb12298118c5a5332e
hoferbao <hoferbao@gmail.com>
2018-11-28 20:36:01 +0800

    two sum

--- src/main/java/leetcode/p001/TwoSum.java
+++ src/main/java/leetcode/p001/TwoSum.java
@@ -10,8 +10,22 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
-        int[] result = twoSum(nums, target);
+        int[] result = twoSum2(nums, target);
         System.out.println("[" + result[0] + "," + result[1] + "]");
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        int i = 0;
+        int j = 1;
+        for (; i < nums.length - 1; i++) {
+            for (j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+
+        }
+        return new int[]{i, j};
     }
 
     public static int[] twoSum(int[] nums, int target) {
