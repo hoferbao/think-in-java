5f98f430e6cc2029de9093025fd8a8403e283b76
hoferbao <hoferbao@gmail.com>
2019-08-05 10:15:20 +0800

    final test

---
+++ src/test/java/com/hofer/tij/common/Test.java
@@ -0,0 +1,14 @@
+package com.hofer.tij.common;
+
+/**
+ * @author hofer.bhf
+ * created on 2019/8/5 10:14 AM
+ */
+public class Test {
+    public static void main(String[] args) {
+        final StringBuffer strConst = new StringBuffer();
+        strConst.append("a");
+        //strConst = null;
+
+    }
+}
