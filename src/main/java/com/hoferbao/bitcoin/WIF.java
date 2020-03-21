9a2bb9b4dd8ff03ea55c7344f274f39bad1e9a4b
hoferbao@gmail.com <key7417gh>
2020-03-21 20:15:18 +0800

    btc

---
+++ src/main/java/com/hoferbao/bitcoin/WIF.java
@@ -0,0 +1,35 @@
+package com.hoferbao.bitcoin;
+
+import org.bouncycastle.crypto.digests.SHA256Digest;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class WIF {
+    public static String generatePrivateKeyWIF(String string1 ) throws NoSuchAlgorithmException {
+        String string = "80" + string1;
+        byte[] data = hexStringToByteArray(string);
+        byte[] digest = MessageDigest.getInstance("SHA-256").digest(hexStringToByteArray(string));
+        String result = bytesToHex(digest);
+        System.out.println(result);
+        return result;
+    }
+
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        return result.toString();
+    }
+}
---
+++ src/main/java/com/hoferbao/bitcoin/Wallet.java
@@ -0,0 +1,155 @@
+package com.hoferbao.bitcoin;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.interfaces.ECPrivateKey;
+
+/**
+ * https://www.kongyixueyuan.com/witbook/4/33
+ *
+ * @author hanru
+ */
+@AllArgsConstructor
+@Data
+public class Wallet implements Serializable {
+
+    private static final long serialVersionUID = 6796631943459965436L;
+
+    /**
+     * 校验码长度
+     */
+    private static final int ADDRESS_CHECKSUM_LEN = 4;
+    /**
+     * 私钥
+     */
+    private BCECPrivateKey privateKey;
+
+    public BCECPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(BCECPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public byte[] getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(byte[] publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    /**
+     * 公钥
+     */
+    private byte[] publicKey;
+
+    /**
+     * 初始化钱包
+     */
+    private void initWallet() {
+        try {
+            KeyPair keyPair = newECKeyPair();
+            BCECPrivateKey privateKey = (BCECPrivateKey) keyPair.getPrivate();
+            BCECPublicKey publicKey = (BCECPublicKey) keyPair.getPublic();
+
+            byte[] publicKeyBytes = publicKey.getQ().getEncoded(false);
+
+            this.setPrivateKey(privateKey);
+            this.setPublicKey(publicKeyBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建新的密钥对
+     *
+     * @return
+     * @throws Exception
+     */
+    private KeyPair newECKeyPair() throws Exception {
+        // 注册 BC Provider
+        Security.addProvider(new BouncyCastleProvider());
+        // 创建椭圆曲线算法的密钥对生成器，算法为 ECDSA
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", BouncyCastleProvider.PROVIDER_NAME);
+        // 椭圆曲线（EC）域参数设定
+        // bitcoin 为什么会选择 secp256k1，详见：https://bitcointalk.org/index.php?topic=151120.0
+        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("secp256k1");
+        keyPairGenerator.initialize(ecSpec, new SecureRandom());
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    public Wallet() {
+        initWallet();
+    }
+
+    /**
+     * 获取钱包地址
+     *
+     * @return
+     */
+    public String getAddress() {
+        try {
+            // 1. 获取 ripemdHashedKey
+            byte[] ripemdHashedKey = AddressUtils.ripeMD160Hash(this.getPublicKey());
+
+            // 2. 添加版本 0x00
+            ByteArrayOutputStream addrStream = new ByteArrayOutputStream();
+            addrStream.write((byte) 0);
+            addrStream.write(ripemdHashedKey);
+            byte[] versionedPayload = addrStream.toByteArray();
+
+            // 3. 计算校验码
+            byte[] checksum = AddressUtils.checksum(versionedPayload);
+
+            // 4. 得到 version + paylod + checksum 的组合
+            addrStream.write(checksum);
+            byte[] binaryAddress = addrStream.toByteArray();
+
+            // 5. 执行Base58转换处理
+            return Base58Check.rawBytesToBase58(binaryAddress);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException("Fail to get wallet address ! ");
+    }
+
+
+    private static String adjustTo64(String s) {
+        switch (s.length()) {
+            case 62:
+                return "00" + s;
+            case 63:
+                return "0" + s;
+            case 64:
+                return s;
+            default:
+                throw new IllegalArgumentException("not a valid key: " + s);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Wallet wallet = new Wallet();
+        System.out.println("address = " + wallet.getAddress());
+        ECPrivateKey privateKey = wallet.getPrivateKey();
+        String privateKeyStr = adjustTo64(privateKey.getS().toString(16)).toUpperCase();
+        String privateKeyStrWif = WIF.generatePrivateKeyWIF(privateKeyStr);
+        System.out.println("privateKey = " + privateKeyStrWif);
+    }
+
+}
