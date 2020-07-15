476e162ad817bab7f722ac5eba87a599e5db0714
hofer <hongfei.bhf@alibaba-inc.com>
2020-07-15 21:00:38 +0800

    delete

--- src/main/java/com/hofer/tij/common/SandBox.java
+++ src/main/java/com/hofer/tij/common/SandBox.java
@@ -1,11 +1,18 @@
 package com.hofer.tij.common;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author hofer.bhf
  * created on 2018/05/19 5:28 PM
  */
 public class SandBox {
     public static void main(String[] args) {
-        System.out.println(1 << 2);
+        List<Long> x = new ArrayList<>();
+        x.add(1L);
+        x.add(999L);
+        System.out.println(x.stream().map(Object::toString).collect(Collectors.toList()));
     }
 }
